d622da6d6b8e5c55ce11d146c6c3ad52c7db22f0
priyank vaghani <vaghanipriyank99@gmail.com>
2025-07-30 17:04:15 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,53 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { connectDb } from "./config/connectDB.js";
 import userRoutes from "./routes/user.routes.js";
 import sellerRoutes from "./routes/seller.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import { connectCloudinary } from "./config/cloudinary.js";
-import cartRoutes from "./routes/cart.routes.js"
+import cartRoutes from "./routes/cart.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import addressRoutes from "./routes/address.routes.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
-const allowedOrigins = [process.env.CORS_ORIGIN]; 
+const PORT = process.env.PORT || 3000;
+
+// Database and Cloudinary connect
 connectDb();
 connectCloudinary();
-//Middleware
+
+// Middleware
 app.use(express.json());
-app.use(cors({
-  origin: function(origin, callback){
-    if (!origin) {
-      return callback(null, true); // Allow requests like Postman or file://
-    }
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      console.warn("Blocked by CORS:", origin);
-      return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
-    }
-  },
-  credentials: true,
-}));
+app.use(cookieParser());
 
-
+// Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the Grocery API");
-})
-app.use(cookieParser());
-app.use("/images", express.static("uploads"));
+});
 app.use("/api/user", userRoutes);
 app.use("/api/seller", sellerRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/address", addressRoutes);
-app.listen(PORT, () => console.log("Server is running on port 3000"));
+
+// ------------------------ Serve React Static Files ------------------------
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from React Vite build
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+// -------------------------------------------------------------------------
+
+// Start Server
+app.listen(PORT, () => console.log("Server is running on port", PORT));
