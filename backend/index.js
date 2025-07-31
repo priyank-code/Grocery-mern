24d8dc89f3f3add1f3c445c998d0810f453ffd5a
priyank vaghani <vaghanipriyank99@gmail.com>
2025-07-31 12:39:06 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ connectDb();
 connectCloudinary();
 
 app.use(cors({
-  origin: true,
+  origin: "https://grocery-mern.onrender.com",
   credentials: true,
 }));
 
@@ -45,10 +45,10 @@ app.use("/api/address", addressRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
 
 // Start Server
